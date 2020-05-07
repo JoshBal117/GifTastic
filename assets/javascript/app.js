b2562adc5b70f126df33999bca0574fcc3644552
Joshua Balao <joshuacbalao@gmail.com>
2020-05-06 23:30:05 -0400

    created code to pull images from giphy, create function to have images be still until user interacts with image; and polished up HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,54 @@
 $('document').ready(function() {
 
 
-/*   This is going to be my inital  empty array of giphs to be pulled. */
-      let topic = []
+/*   This is going to be my inital array of topics-Heroes */
+      let topic = [
+        "Avengers",
+        "Justice League",
+        "Kamen Rider",
+        "Power Rangers",
+        "Master Chief",
+        "Goku",
+        "One Punch Man",
+      ];
 
     /* these are going to be the rest of the vairables for the app */
 
-    const key = "RvbjeZv71Wbn8T8UwLoknIV7LB7nCZOL";
+
+  
+
+
+    let search = $(this).data("search")
+    console.log(search);
+
+    const apiKey = "RvbjeZv71Wbn8T8UwLoknIV7LB7nCZOL";
     const q;
-    let queryURL: "https://api.giphy.com/v1/gifs/search?api_keys=" + key + "&q=" +
-    console.log(queryURL);
+    
 
     function displayHeroes() {
-        $.ajax({
+      let queryURL = "https://api.giphy.com/v1/gifs/search?api_key" + apiKey + "search" + "&limit=10&offset=08rating=PG&lang=en"
+    console.log(queryURL);
+        $.ajax({ /* this is the ajax request tht will pull the giphs from giphy using the API */
             url: queryURL,
             method: "GET"
-          }).then(function(response){
+          }).then(function(response){/*this is a loop that will go through the array and add the corrct rating*/
+            console.log(response)
+            for (let i = 0; i < response.data.length; i++) {
+              let giphs = response.data[i].images.fixed_height.url;
+              let still = response.data[i].images.fixed_height_still.url;
+              let heroDiv = $("<div class='Hero'>");
+              let rating = $("<p>").text("Rating: " + response.data[i].rating);
+              let heroImage = $("<img>"); 
+              heroImage.attr("data-giph", giph);
+              heroImage.attr("data-still", still);
+              heroImage.attr("data-state", giph)
+              heroImage.addClass("heroImage")
+              eroImage.attr("src", response.data[i].images.fixed_height.url);
+              heroDiv.append(heroImage);
+              heroDiv.append(rating)
+              $("#addedHeroes").prepend(heroDiv);
               
-            let results = Response.data;
-            console.log(results)
+            }
         })
     }
 
@@ -41,7 +71,7 @@ $('document').ready(function() {
 
 
     $(".gif").on("click", function() {
-        var state = $(this).attr("data-state");
+        let state = $(this).attr("data-state");
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
