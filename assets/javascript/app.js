ad5e94c51f25490ac694d91fa90ea85901b3b1d3
Joshua Balao <joshuacbalao@gmail.com>
2020-05-10 00:19:38 -0400

    afinalaized style and finalized html, js needs to be refined

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 $('document').ready(function() {
+  console.log("ready, set go!!")
 
 
 /*   This is going to be my inital array of topics-Heroes */
-      let topic = [
+      let topicHeroes = [
         "Avengers",
         "Justice League",
         "Kamen Rider",
@@ -10,40 +11,52 @@ $('document').ready(function() {
         "Master Chief",
         "Goku",
         "One Punch Man",
+        
       ];
 
     /* these are going to be the rest of the vairables for the app */
 
-
+    function renderButtons() {
+      $("#addedButtons").empty();
+      for (let i = 0; i < topicHeroes.length; i++) {
+        var heroButtons =$("<button>")
+        heroButtons.addClass("hero-btn");
+        heroButtons.attr("data-name", topicHeroes[i]);
+        heroButtons.text(topicHeroes[i]);
+        $("#addedButtons").append(heroButtons);
+     }
+    }; 
   
-
-
-    let search = $(this).data("search")
-    console.log(search);
-
-    const apiKey = "RvbjeZv71Wbn8T8UwLoknIV7LB7nCZOL";
-    const q;
+  
     
 
-    function displayHeroes() {
-      let queryURL = "https://api.giphy.com/v1/gifs/search?api_key" + apiKey + "search" + "&limit=10&offset=08rating=PG&lang=en"
-    console.log(queryURL);
+    let search = $(this).data("search")
+    console.log("search, ready");
+
+    const apiKey = "RvbjeZv71Wbn8T8UwLoknIV7LB7nCZOL";
+    var q = $(this).attr("data-name");
+    
+
+    function displayHeroes(heroName) {
+      console.log("heroName")
+      let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey +  "&q=" + q +  "&limit=10&offset=08rating=PG&lang=en"
         $.ajax({ /* this is the ajax request tht will pull the giphs from giphy using the API */
             url: queryURL,
             method: "GET"
           }).then(function(response){/*this is a loop that will go through the array and add the corrct rating*/
             console.log(response)
+                //this is the loop that will pull data from the giphy website
             for (let i = 0; i < response.data.length; i++) {
-              let giphs = response.data[i].images.fixed_height.url;
+              let gif = response.data[i].images.fixed_height.url;
               let still = response.data[i].images.fixed_height_still.url;
               let heroDiv = $("<div class='Hero'>");
               let rating = $("<p>").text("Rating: " + response.data[i].rating);
               let heroImage = $("<img>"); 
-              heroImage.attr("data-giph", giph);
+              heroImage.attr("data-gif", gif);
               heroImage.attr("data-still", still);
-              heroImage.attr("data-state", giph)
+              heroImage.attr("data-state", gif)
               heroImage.addClass("heroImage")
-              eroImage.attr("src", response.data[i].images.fixed_height.url);
+              heroImage.attr("src", response.data[i].images.fixed_height.url);
               heroDiv.append(heroImage);
               heroDiv.append(rating)
               $("#addedHeroes").prepend(heroDiv);
@@ -52,47 +65,37 @@ $('document').ready(function() {
         })
     }
 
+    $('#add-hero').click(function(event) {
+      event.preventDefault();
+      let hero = $('#hero-input').val().trim();
+      if (topicHeroes.length > 2) {
+        topicHeroes.push(topicHeroes)
+      } 
+      renderButtons();
+    });
 
-    function renderButton() {
-        $("#giphDisplay").empty();
+    $(document).on("click", ".hero-btn", function(){
+      var heroName = $(this).data("name");
+      console.log("heroName")
+      console.log($(this));
+      displayHeroes(heroName);
 
-        for (let i = 0; i < giphs.length; i++) {
+
+
+
+
+
+    $(document).on("click", ".heroImage", function() {
+        let image = $(this)
         
-            let a = $("<buttons>");
-            a.addClass("image");
-            a.attr("data-name", images[i]);
-            a.text(images[i]);
-            $("giphDisplay").append(a);
-            
-        }
-    }
-
-
-
-
-    $(".gif").on("click", function() {
-        let state = $(this).attr("data-state");
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
+        if (image.attr('data-state') === 'gif') {
+            image.attr('src', image.attr('data-still')); 
+            image.attr('data-state', 'still');
           } else {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
+            image.attr('src', image.attr('data-gif'));
+            image.attr('data-state', 'gif');
           }
         });
     
-    
-    
-    
-    
-    
-    
-    
-
-     
-
-
+    });
 })
-
-
-
